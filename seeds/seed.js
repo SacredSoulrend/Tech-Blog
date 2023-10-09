db6157caffd3cbb29502825b8a9895b2649b6664
Son Giang <sgiangt@gmail.com>
2023-10-08 23:17:38 -0600

    added seed data for comments, post and user

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentsData = require('./commentsData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await Post.bulkCreate(postData);
+
+  for (const comment of commentsData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
